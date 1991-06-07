b6220bfb729907bc7e3d0ad704b33e9c76f0af2c
Thomas E. Dickey <dickey@invisible-island.net>
1991-06-07 16:43:31 -0400

    snapshot of project "td_lib", label t19910607

--- src/pathname/ftype2.c
+++ src/pathname/ftype2.c
@@ -1,5 +1,5 @@
 #ifndef	lint
-static	char	Id[] = "$Id: ftype2.c,v 8.1 1991/05/15 09:25:41 dickey Exp $";
+static	char	Id[] = "$Id: ftype2.c,v 9.0 1991/06/07 11:56:13 ste_cm Rel $";
 #endif
 
 /*
@@ -7,36 +7,17 @@ static	char	Id[] = "$Id: ftype2.c,v 8.1 1991/05/15 09:25:41 dickey Exp $";
  * Author:	T.E.Dickey
  * Created:	13 Sep 1988
  * $Log: ftype2.c,v $
- * Revision 8.1  1991/05/15 09:25:41  dickey
- * apollo sr10.3 cpp complains about tag in #endif
+ * Revision 9.0  1991/06/07 11:56:13  ste_cm
+ * BASELINE Mon Jun 10 10:09:56 1991 -- apollo sr10.3
  *
+ *		Revision 8.2  91/06/07  11:56:13  dickey
+ *		*** empty log message ***
+ *		
+ *		Revision 8.1  91/05/15  09:25:41  dickey
+ *		apollo sr10.3 cpp complains about tag in #endif
+ *		
  *		Revision 8.0  89/10/04  12:11:05  ste_cm
  *		BASELINE Mon Aug 13 15:06:41 1990 -- LINCNT, ADA_TRANS
- *		
- *		Revision 7.0  89/10/04  12:11:05  ste_cm
- *		BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
- *		
- *		Revision 6.0  89/10/04  12:11:05  ste_cm
- *		BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
- *		
- *		Revision 5.0  89/10/04  12:11:05  ste_cm
- *		BASELINE Fri Oct 27 12:27:25 1989 -- apollo SR10.1 mods + ADA_PITS 4.0
- *		
- *		Revision 4.1  89/10/04  12:11:05  dickey
- *		lint (apollo SR10.1)
- *		
- *		Revision 4.0  88/09/13  06:46:55  ste_cm
- *		BASELINE Thu Aug 24 09:38:55 EDT 1989 -- support:navi_011(rel2)
- *		
- *		Revision 3.0  88/09/13  06:46:55  ste_cm
- *		BASELINE Mon Jun 19 13:27:01 EDT 1989
- *		
- *		Revision 2.0  88/09/13  06:46:55  ste_cm
- *		BASELINE Thu Apr  6 09:45:13 EDT 1989
- *		
- *		Revision 1.2  88/09/13  06:46:55  dickey
- *		sccs2rcs keywords
- *		
  *
  * Function:	returns a pointer to the right-most "."-suffix of the leaf of
  *		the given pathname.
