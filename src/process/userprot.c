ae9d2de3cc98a5f0e2baae379af14697bb9ce06a
Thomas E. Dickey <dickey@invisible-island.net>
1991-07-11 14:43:23 -0400

    snapshot of project "td_lib", label t19910711

--- src/process/userprot.c
+++ src/process/userprot.c
@@ -1,42 +1,14 @@
 #ifndef	lint
-static	char	Id[] = "$Id: userprot.c,v 9.0 1991/05/15 09:57:23 ste_cm Rel $";
+static	char	Id[] = "$Id: userprot.c,v 9.1 1991/07/11 14:43:23 dickey Exp $";
 #endif
 
 /*
  * Title:	userprot.c (set protection for setuid program's caller)
  * Author:	T.E.Dickey
  * Created:	30 Aug 1988
- * $Log: userprot.c,v $
- * Revision 9.0  1991/05/15 09:57:23  ste_cm
- * BASELINE Mon Jun 10 10:09:56 1991 -- apollo sr10.3
- *
- *		Revision 8.1  91/05/15  09:57:23  dickey
- *		apollo sr10.3 cpp complains about tag in #endif
- *		
- *		Revision 8.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Mon Aug 13 15:06:41 1990 -- LINCNT, ADA_TRANS
- *		
- *		Revision 7.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
- *		
- *		Revision 6.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
- *		
- *		Revision 5.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Fri Oct 27 12:27:25 1989 -- apollo SR10.1 mods + ADA_PITS 4.0
- *		
- *		Revision 4.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Thu Aug 24 09:38:55 EDT 1989 -- support:navi_011(rel2)
- *		
- *		Revision 3.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Mon Jun 19 13:27:01 EDT 1989
- *		
- *		Revision 2.0  88/09/13  09:54:18  ste_cm
- *		BASELINE Thu Apr  6 09:45:13 EDT 1989
- *		
- *		Revision 1.3  88/09/13  09:54:18  dickey
- *		sccs2rcs keywords
- *		
+ * Modified:
+ *		11 Jul 1991, if effective-user is root, set ownership and act
+ *			     within the current process.
  *		13 Sep 1988, rewrote using 'for_user()'
  *
  * Function:	For a set-uid program, sets protection (and modification time)
@@ -47,6 +19,7 @@ static	char	Id[] = "$Id: userprot.c,v 9.0 1991/05/15 09:57:23 ste_cm Rel $";
  */
 
 #include	"ptypes.h"
+extern	int	errno;
 
 static	char	*upr_name;
 static	int	upr_mode;
@@ -66,5 +39,12 @@ time_t	mtime;
 	upr_name = name;
 	upr_mode = mode;
 	upr_time = mtime;
+	if (!geteuid()) {
+		if (chown(upr_name, getuid(), getgid()) < 0)
+			return (-1);
+		errno = 0;
+		upr_func();
+		return (errno != 0) ? -1 : 0;
+	}
 	return (for_user(upr_func));
 }
