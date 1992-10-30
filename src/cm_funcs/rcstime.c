aeaea8505dd55a400066825105973d3a74c6b769
Thomas E. Dickey <dickey@invisible-island.net>
1992-10-30 08:14:11 -0500

    snapshot of project "td_lib", label t19921030

--- src/cm_funcs/rcstime.c
+++ src/cm_funcs/rcstime.c
@@ -1,11 +1,13 @@
 #ifndef	lint
-static	char	*Id = "$Id: rcstime.c,v 11.1 1992/10/27 08:15:44 dickey Exp $";
+static	char	*Id = "$Id: rcstime.c,v 11.2 1992/10/30 07:15:28 dickey Exp $";
 #endif
 
 /*
  * Title:	rcstime.c
  * Author:	T.E.Dickey
  * Created:	05 Feb 1992
+ * Modified:
+ *		30 Oct 1992, RCS version 5 uses GMT, not local-time
  *
  * Function:	converts to/from string/time_t values for RCS archives.
  */
@@ -15,49 +17,43 @@ static	char	*Id = "$Id: rcstime.c,v 11.1 1992/10/27 08:15:44 dickey Exp $";
 #include <time.h>
 
 #if	RCS_VERSION >= 5
-#define	RCS_ZONE	0	/* use GMT */
+#define	RCS_ZONE(t)	gmt_offset(t)
 #else
-#define	RCS_ZONE	5	/* I am in EST5EDT... */
+#define	RCS_ZONE(t)	0	/* no offset, use local-time */
 #endif
 
 /*
  * Decode an rcs archive-date
  */
-time_t
-rcs2time(
-_AR1(char *,	from))
-_DCL(char *,	from)
+time_t	rcs2time(
+	_AR1(char *,	from))
+	_DCL(char *,	from)
 {
 	time_t	the_time = 0;
 	int	year, mon, day, hour, min, sec;
 
-	newzone(RCS_ZONE,0,FALSE);
-
 	if (sscanf(from, FMT_DATE, &year, &mon, &day, &hour, &min, &sec) == 6)
 		the_time = packdate(1900+year, mon, day, hour, min, sec);
 
-	oldzone();
-	return the_time;
+	return the_time - RCS_ZONE(the_time);
 }
 
 /*
  * Convert a unix time to an rcs archive-date
  */
-void
-time2rcs(
-_ARX(char *,	to)
-_AR1(time_t,	from)
-	)
-_DCL(char *,	to)
-_DCL(time_t,	from)
+void	time2rcs(
+	_ARX(char *,	to)
+	_AR1(time_t,	from)
+		)
+	_DCL(char *,	to)
+	_DCL(time_t,	from)
 {
 	struct	tm *t;
 
-	newzone(RCS_ZONE,0,FALSE);
+	from += RCS_ZONE(from);
 	t = localtime(&from);
 	FORMAT(to, FMT_DATE,
 		t->tm_year, t->tm_mon + 1,
 		t->tm_mday, t->tm_hour,
 		t->tm_min,  t->tm_sec);
-	oldzone();
 }
