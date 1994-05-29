b20e68bd8c4019f2dcec73e86cfb5e4ff36509d2
Thomas E. Dickey <dickey@invisible-island.net>
1994-05-29 00:50:54 -0400

    snapshot of project "td_lib", label t19940529

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,5 @@
 /* acconfig.h
- * $Header: /users/source/archives/td_lib.vcs/RCS/acconfig.h,v 12.3 1994/05/28 00:43:54 tom Exp $
+ * $Header: /users/source/archives/td_lib.vcs/RCS/acconfig.h,v 12.5 1994/05/29 00:50:54 tom Exp $
  *
  * Extended symbols for generating configuration scripts for td_lib with
  * autoconf.
@@ -16,6 +16,15 @@
 
 /* Define if preprocessor does "/**/" concatenation */
 #undef HAVE_OLD_TOKEN_SPLICE
+
+/* Define if preprocessor uses leading '#' to change macro arg to quotes */
+#undef HAVE_NEW_TOKEN_QUOTE
+
+/* Define if preprocessor substitutes macro arguments inside quotes */
+#undef HAVE_OLD_TOKEN_QUOTE
+
+/* Define if we have <regex.h> with regexec/regcomp/regfree interface */
+#undef HAVE_REGEX_H_FUNCS
 
 /* Define if <time.h> does declare 'timezone' */
 #undef TIMEZONE_DECLARED
