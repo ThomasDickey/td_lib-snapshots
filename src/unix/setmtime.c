6faeb25d4bd75c97be53dcc0f50889784156efdb
Thomas E. Dickey <dickey@invisible-island.net>
1995-04-22 21:09:55 -0400

    snapshot of project "td_lib", label t19950422

--- src/unix/setmtime.c
+++ src/unix/setmtime.c
@@ -1,12 +1,9 @@
-#if	!defined(NO_IDENT)
-static	char	Id[] = "$Id: setmtime.c,v 12.5 1994/05/23 23:45:16 tom Exp $";
-#endif
-
 /*
  * Title:	setmtime.c (set file's mtime)
  * Author:	T.E.Dickey
  * Created:	20 May 1988
  * Modified:
+ *		22 Apr 1995, added 'atime' parameter.
  *		29 Oct 1993, ifdef-ident, port to HP/UX
  *		21 Sep 1993, gcc-warnings
  *		03 Oct 1991, convert to ANSI
@@ -20,6 +17,8 @@ static	char	Id[] = "$Id: setmtime.c,v 12.5 1994/05/23 23:45:16 tom Exp $";
 #include	"ptypes.h"
 #include	<time.h>
 
+MODULE_ID("$Id: setmtime.c,v 12.7 1995/04/22 20:52:46 tom Exp $")
+
 #if HAVE_UTIME_H
 #include	<utime.h>
 #else
@@ -28,14 +27,17 @@ extern	int	utime(_arx(const char *,s) _ar1(const struct utimbuf *,p));
 #endif
 
 int	setmtime(
-	_ARX(char *,	name)			/* name of file to touch */
-	_AR1(time_t,	mtime)			/* time we want to leave */
+	_ARX(char *,	name)		/* name of file to touch */
+	_ARX(time_t,	mtime)		/* modification time we want to leave */
+	_AR1(time_t,	atime)		/* access time we want to leave */
 		)
 	_DCL(char *,	name)
 	_DCL(time_t,	mtime)
+	_DCL(time_t,	atime)
 {
 	struct utimbuf tp;
 	tp.actime  = time((time_t *)0);
 	tp.modtime = mtime;
+	tp.actime  = atime;
 	return (utime(name, &tp));
 }
