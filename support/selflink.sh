b158bff1448b5c35a43b003edbe65cfaded918db
Thomas E. Dickey <dickey@invisible-island.net>
2004-03-22 00:31:46 -0500

    snapshot of project "td_lib", label t20040321

--- support/selflink.sh
+++ support/selflink.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-# $Id: selflink.sh,v 12.1 2004/03/07 15:33:00 tom Exp $
+# $Id: selflink.sh,v 12.2 2004/03/22 00:31:46 tom Exp $
 # Ensure that the "td_lib" name points to the top of this directory tree,
 # making it simple to build ded, cm_tools and sccs_tools side-by-side.
 # A symbolic link is acceptable for this purpose.
 CDPATH=
-case $PWD in
+case `pwd` in
 */td_lib)
 	;;
 *)
 	test -d ../td_lib	&& rm -f ../td_lib
 	test -d ../td_lib 	&& exit 1
-	ln -s $PWD ../td_lib	|| exit 1
+	ln -s `pwd` ../td_lib	|| exit 1
 	;;
 esac
